23e4f2d5eff5e146af4c506ab22cec0cf36cec6c
Peter <apppeterpan@gmail.com>
2023-12-27 21:17:33 +0800

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by SHIH-YING PAN on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
